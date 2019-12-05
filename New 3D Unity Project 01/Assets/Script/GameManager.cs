048b60fdeac40e5de30a0bb9da3850c65c1aa820
beliel1313 <55227114+beliel1313@users.noreply.github.com>
2019-12-05 20:08:08 +0800

    Title Building

---
+++ New 3D Unity Project 01/Assets/Script/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;     // Use audio API.
+using UnityEngine.UI;       // Use UI API.
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public AudioMixer mixer;
+    public Text loadingText;
+    public Slider loadingBar;
+
+    public void BGM_vol (float value)  {mixer.SetFloat("vBGM", value);}
+    public void SFX_vol (float value)  {mixer.SetFloat("vSFX", value);}
+    public void Play()
+    {
+        SceneManager.LoadScene("NewScene01");
+        StartCoroutine(Loading());
+    }
+    private IEnumerator Loading()
+    {
+        //  print("Test 1");
+        //  yield return new WaitForSeconds(1);
+        //  print("Test 2");
+        AsyncOperation ao = SceneManager.LoadSceneAsync("NewScene01");
+        ao.allowSceneActivation = false;
+        while (ao.isDone == false)
+        {
+            loadingText.text ="LOADING " + ((ao.progress / 0.9f) * 100).ToString() + " %";
+            loadingBar.value = ao.progress / 0.9f;
+            yield return new WaitForSeconds(0.025f);
+            if (ao.progress == 0.9f && Input.anyKey)  ao.allowSceneActivation = true;
+        }
+    }
+}
