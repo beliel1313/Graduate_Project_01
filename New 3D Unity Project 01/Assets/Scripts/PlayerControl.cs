15b86f3d08027d5a8477e3de96e627832fc6215e
beliel1313 <55227114+beliel1313@users.noreply.github.com>
2020-03-02 15:42:59 +0800

    200302

--- New 3D Unity Project 01/Assets/Scripts/PlayerControl.cs
+++ New 3D Unity Project 01/Assets/Scripts/PlayerControl.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
+    private Rigidbody2D rig;   //刚体
+    private float horizontal;  //水平偏移量
+    private float move; //水平移动速度（左 或 右）
+    [Header("MoveValue")]
+    public float moveSpeed = 5.5f; //水平移动速度绝对值
+    public float jumpForce = 300f;  //跳跃的力
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.Translate(Vector2.right * Time.deltaTime*2);
+
+    void Start() {
+        rig = GetComponent<Rigidbody2D>();   //获取主角刚体组件
+
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {    //如果按下空格 
+            rig.AddForce(new Vector2(0, jumpForce));   //给刚体一个向上的力
+            horizontal = Input.GetAxis("Horizontal");   //水平方向按键偏移量
+            move = horizontal * moveSpeed;   //刚体具体速度
+            rig.velocity = new Vector2(move, rig.velocity.y);
+        }
+
+        if (Input.GetButton("Horizontal")) {
+            horizontal = Input.GetAxis("Horizontal");
+            move = horizontal * moveSpeed;   //刚体具体速度
+            rig.velocity = new Vector2(move, rig.velocity.y);
+        }
+
     }
 }
