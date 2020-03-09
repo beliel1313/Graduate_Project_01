5e1838e767cd02b65951f90c1b2dc338ad8c5ef5
beliel1313 <55227114+beliel1313@users.noreply.github.com>
2020-03-09 15:48:42 +0800

    200309

---
+++ New 3D Unity Project 01/Assets/Scripts/PlayerParameter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerParameter : MonoBehaviour {
+
+    [Header("HP")]
+    public float hp;
+    public Slider hpBar;
+
+	// Use this for initialization
+	void Start () {
+        // 賦予角色生命值予血條
+        hpBar.maxValue = hp;
+        hpBar.value = hpBar.maxValue;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            hp -= 10;
+            hpBar.value = hp;
+            Debug.Log(hp);
+        }
+	}
+
+    // 角色碰撞傷害處理
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+}
