b88126c98fc81a3e35f7f6d18007f8ae557adc83
beliel1313 <55227114+beliel1313@users.noreply.github.com>
2020-05-18 15:42:02 +0800

    200518

---
+++ New 3D Unity Project 01/Assets/Scripts/system/ShopBuy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopBuy : MonoBehaviour {
+
+    public Text textBill;
+    private int totalBill;
+
+	// Use this for initialization
+	void Start ()
+    {
+        totalBill = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        textBill.text = "合計 " + totalBill.ToString();
+
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        totalBill += collision.GetComponent<ItemFood>().price;
+        collision.tag = "InCart";
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        totalBill -= collision.GetComponent<ItemFood>().price;
+        collision.tag = "Untagged";
+    }
+
+    public void Buying()
+    {
+        Debug.Log("Buy " + GameObject.FindGameObjectWithTag("InCart").name);
+        Destroy(GameObject.FindGameObjectWithTag("InCart"));
+    }
+}
