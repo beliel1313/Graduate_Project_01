a150dcf2f567f5fc610290503f76dfc2404bd220
beliel1313 <55227114+beliel1313@users.noreply.github.com>
2020-05-22 14:46:48 +0800

    200522

---
+++ New 3D Unity Project 01/Assets/Scripts/system/DragnDrop.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DragnDrop : MonoBehaviour {
+
+    float OffsetX;
+    float OffsetY;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void BeginDrag()
+    {
+        OffsetX = transform.position.x - Input.mousePosition.x; ;
+        OffsetY = transform.position.y - Input.mousePosition.y; ;
+    }
+
+    public void OnDrag()
+    {
+        transform.position = new Vector3(Input.mousePosition.x + OffsetX, Input.mousePosition.y + OffsetY);
+
+    }
+}
--- New 3D Unity Project 01/Assets/Scripts/system/ItemFood.cs
+++ New 3D Unity Project 01/Assets/Scripts/system/ItemFood.cs
@@ -6,12 +6,13 @@ using UnityEngine.UI;
 
 public class ItemFood : MonoBehaviour {
 
+    public string foodName;
     public int price = 0;
 
 
     // Use this for initialization
     void Start () {
-
+        
 	}
 	
 	// Update is called once per frame
