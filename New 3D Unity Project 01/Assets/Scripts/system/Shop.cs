a6beddff0e057c8341a1c4db2c434a61034d9dfe
beliel1313 <55227114+beliel1313@users.noreply.github.com>
2020-05-25 15:54:12 +0800

    200525

---
+++ New 3D Unity Project 01/Assets/Scripts/system/Shop.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop : MonoBehaviour {
+    public Transform cartBoard;
+    public GameObject[] slotFood;
+    public GameObject[] slotTool;
+    public GameObject[] slotGear;
+    public GameObject[] foods;
+    public GameObject[] tools;
+    public GameObject[] gears;
+
+
+    // Use this for initialization
+    void Start () {
+        for (int i = 0; i < foods.Length; i++)
+        {
+            Instantiate(foods[i].gameObject, slotFood[i].transform, false);
+            Debug.Log(foods[i].name + " " + foods[i].transform.position);
+
+        }
+
+        for (int i = 0; i < tools.Length; i++)
+        {
+            Instantiate(tools[i].gameObject, slotTool[i].transform, false);
+            Debug.Log(tools[i].name + " " + tools[i].transform.position);
+
+        }
+
+        for (int i = 0; i < gears.Length; i++)
+        {
+            Instantiate(gears[i].gameObject, slotGear[i].transform, false);
+            Debug.Log(gears[i].name + " " + gears[i].transform.position);
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+	}
+
+    public void PutInCart() 
+    {
+
+    }
+}
--- New 3D Unity Project 01/Assets/Scripts/system/ShopBuy.cs
+++ New 3D Unity Project 01/Assets/Scripts/system/ShopBuy.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ShopBuy : MonoBehaviour {
+    [Header("Master")]
+    public RectTransform cartBoard;
 
+    [Header("Shop Food")]
+    public GameObject[] foods;
+
+    [Header("Shop Tool")]
+    public GameObject[] tools;
+
+    [Header("Shop Gear")]
+    public GameObject[] gears;
+
+    [Header("GUI")]
     public Text textBill;
     private int totalBill;
 
 	// Use this for initialization
-	void Start ()
-    {
+	void Start () {
         totalBill = 0;
+
 	}
 	
 	// Update is called once per frame
-	void Update ()
-    {
+	void Update () {
         textBill.text = "合計 " + totalBill.ToString();
 
 	}
@@ -25,17 +36,25 @@ public class ShopBuy : MonoBehaviour {
     {
         totalBill += collision.GetComponent<ItemFood>().price;
         collision.tag = "InCart";
+
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         totalBill -= collision.GetComponent<ItemFood>().price;
         collision.tag = "Untagged";
+
+    }
+
+    public void PutInCart()
+    {
+
     }
 
     public void Buying()
     {
         Debug.Log("Buy " + GameObject.FindGameObjectWithTag("InCart").name);
         Destroy(GameObject.FindGameObjectWithTag("InCart"));
+        
     }
 }
