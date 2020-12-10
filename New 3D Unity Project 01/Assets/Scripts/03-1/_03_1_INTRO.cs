da0f89755954f4ec7f339200a337dc7886bc8316
beliel1313 <55227114+beliel1313@users.noreply.github.com>
2020-12-10 11:52:43 +0800

    201210

---
+++ New 3D Unity Project 01/Assets/Scripts/03-1/_03_1_INTRO.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class _03_1_INTRO : MonoBehaviour {
+    public GameObject introMsg, introTxt;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            print("player in");
+            InvokeRepeating("FadeIn", 0f, 0.025f);
+            Destroy(this.gameObject.GetComponent<BoxCollider2D>());
+
+        }
+    }
+
+    void FadeIn()
+    {
+        if (introMsg.GetComponent<Image>().color.a <= 0.5f) 
+        {
+            introMsg.GetComponent<Image>().color += new Color(0, 0, 0, 0.1f);
+        }
+        if (introTxt.GetComponent<Text>().color.a <= 1f)
+        {
+            introTxt.GetComponent<Text>().color += new Color(0, 0, 0, 0.5f);
+        }
+        if (introMsg.GetComponent<Image>().color.a >= 0.5f && introTxt.GetComponent<Text>().color.a >= 1f) 
+        {
+            CancelInvoke("FadeIn");
+            InvokeRepeating("FadeOut", 1f, 0.025f);
+        }
+    }
+
+    void FadeOut()
+    {
+        if (introMsg.GetComponent<Image>().color.a > 0f)
+        {
+            introMsg.GetComponent<Image>().color -= new Color(0, 0, 0, 0.1f);
+        }
+        if (introTxt.GetComponent<Text>().color.a > 0f)
+        {
+            introTxt.GetComponent<Text>().color -= new Color(0, 0, 0, 0.5f);
+        }
+        if (introMsg.GetComponent<Image>().color.a <= 0f && introTxt.GetComponent<Text>().color.a <= 0f) 
+        {
+            CancelInvoke("FadeOut");
+        }
+
+    }
+}
