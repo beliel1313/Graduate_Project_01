5567515a4f556b35350dad4d6ca22b119b4dcc5c
beliel1313 <55227114+beliel1313@users.noreply.github.com>
2020-12-21 14:51:12 +0800

    201221

--- New 3D Unity Project 01/Assets/Scripts/02_WasteRecyle/WasteObject.cs
+++ New 3D Unity Project 01/Assets/Scripts/02_WasteRecyle/WasteObject.cs
@@ -1,16 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class WasteObject : MonoBehaviour {
+public class WasteObject : MonoBehaviour,IPointerDownHandler,IPointerExitHandler,IPointerUpHandler {
+    public int type;
+    private bool isHold;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (isHold == true)
+        {
+            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
 	}
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        // throw new System.NotImplementedException();
+        isHold = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // throw new System.NotImplementedException();
+        isHold = false;
+
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        // throw new System.NotImplementedException();
+        isHold = false;
+
+    }
 }
