adebf2d5d50e3c2ce44097f41048c20d7cf3105f
beliel1313 <55227114+beliel1313@users.noreply.github.com>
2020-12-21 16:41:59 +0800

    201221

--- New 3D Unity Project 01/Assets/Scripts/02_WasteRecyle/_02Manager.cs
+++ New 3D Unity Project 01/Assets/Scripts/02_WasteRecyle/_02Manager.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class _02Manager : MonoBehaviour {
-    public GameObject[] wasteCane;
-    public Transform genPoint;
+    public GameManager gameManager;
+    public GameObject[] waste;
+    public RectTransform genArea;
 
     public Text topMsg;
 
 	// Use this for initialization
 	void Start () {
-		
+        gameManager.GamePause();
+
+        GenWastes();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void GenWastes()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            // genArea.transform.position.x
+            // genArea.transform.position.y
+
+            float groundX = genArea.transform.position.x +
+                Random.Range(genArea.rect.xMin, genArea.rect.xMax);
+            float groundY = genArea.transform.position.y +
+                Random.Range(genArea.rect.yMin, genArea.rect.yMax);
+            int r = Random.Range(0, waste.Length-1);
+            Instantiate(waste[r],
+                new Vector3(groundX, groundY, genArea.transform.position.z),
+                Quaternion.identity, genArea);
+        }
+
+    }
 }
