37c67f313477b733ab2d36b6dd7addf350c24ac1
beliel1313 <55227114+beliel1313@users.noreply.github.com>
2021-03-15 15:40:47 +0800

    210315

---
+++ New 3D Unity Project 01/Assets/Scripts/02-4_ClearSea/_02_4_ObjGen.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _02_4_ObjGen : MonoBehaviour {
+    public Transform[] genPoint;
+    public GameObject[] waste;
+    public float genRate;
+    public float genTimeCount;
+    public float xSpeed;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("GenRate", 0.1f, 0.1f);
+        genRate = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GenRate()
+    {
+        genTimeCount += 0.1f;
+        if (genTimeCount >= genRate)
+        {
+            WasteGen();
+            genTimeCount = 0;
+        }
+    }
+    public void WasteGen()
+    {
+        GameObject wasteClone;
+
+        int rG = Random.Range(0, genPoint.Length);
+        int rW = Random.Range(0, waste.Length);
+
+        xSpeed = Random.Range(1.0f, 5.0f);
+        wasteClone = Instantiate(waste[rW], genPoint[rG]);
+        wasteClone.GetComponent<_02_4_WasteMove>().xSpeed = xSpeed;
+
+    }
+}
---
+++ New 3D Unity Project 01/Assets/Scripts/02-4_ClearSea/_02_4_WasteMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class _02_4_WasteMove : MonoBehaviour,IPointerDownHandler,IPointerUpHandler {
+    public float xSpeed;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(transform.right * xSpeed);
+	}
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+
+    }
+}
