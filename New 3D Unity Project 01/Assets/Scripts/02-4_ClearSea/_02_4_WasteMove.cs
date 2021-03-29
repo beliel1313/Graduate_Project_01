ec13428ef1abe60b17edcda36e0f0f67dd778ba6
beliel1313 <55227114+beliel1313@users.noreply.github.com>
2021-03-29 16:09:39 +0800

    210329

--- New 3D Unity Project 01/Assets/Scripts/02-4_ClearSea/_02_4_WasteMove.cs
+++ New 3D Unity Project 01/Assets/Scripts/02-4_ClearSea/_02_4_WasteMove.cs
@@ -5,24 +5,56 @@ using UnityEngine.EventSystems;
 
 public class _02_4_WasteMove : MonoBehaviour,IPointerDownHandler,IPointerUpHandler {
     public float xSpeed;
+    private bool isHold;
 
     // Use this for initialization
     void Start () {
-		
-	}
+        Invoke("ObjectMove", 0f);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(transform.right * xSpeed);
-	}
+
+        if (isHold == true)
+        {
+            //xSpeed = 0;
+            CancelInvoke("ObjectMove");
+            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+        else
+        {
+            //transform.Translate(transform.right * xSpeed);
+            Invoke("ObjectMove", 0f);
+
+        }
+    }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-
+        isHold = true;
+        //CancelInvoke("ObjectMove");
+        print("hold" + gameObject.name);
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
+        isHold = false;
+        //Invoke("ObjectMove", 0f);
 
     }
+
+    public void ObjectMove()
+    {
+        transform.Translate(transform.right * xSpeed);
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Finish")
+        {
+            Destroy(gameObject);
+        }
+    }
+
 }
